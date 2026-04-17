5e6786afe238edde27494e7bd2f6df0ee094f0da
gscpsa <jimmy@giantsportscards.com>
2026-04-17 16:47:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -97,10 +97,5 @@ def dashboard():
     return render_template("dashboard.html", orders=orders)
 
 if __name__ == "__main__":
-    try:
-        init_db()
-    except Exception as e:
-        print("DB init failed:", e)
-
-    port = int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
